993045bfe5543abf1592f0ae83707d4c89bf88dc
cheeseburgertony <1821955020@qq.com>
2025-01-26 15:45:20 +0800

    feat: 工具栏添加撤销、字体样式等原生StartKit携带的操作

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -1,8 +1,21 @@
 "use client";
 
+import { Separator } from "@/components/ui/separator";
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
-import { LucideIcon, Undo2Icon } from "lucide-react";
+import {
+  BoldIcon,
+  ItalicIcon,
+  ListTodoIcon,
+  LucideIcon,
+  MessageSquarePlusIcon,
+  PrinterIcon,
+  Redo2Icon,
+  RemoveFormattingIcon,
+  SpellCheckIcon,
+  UnderlineIcon,
+  Undo2Icon,
+} from "lucide-react";
 
 interface ToolbarButtonProps {
   onClick?: () => void;
@@ -34,21 +47,102 @@ export const Toolbar = () => {
   const sections: {
     label: string;
     icon: LucideIcon;
-    onClick?: () => void;
+    onClick: () => void;
     isActive?: boolean;
   }[][] = [
     [
       {
         label: "Undo",
         icon: Undo2Icon,
-        onClick: () => editor?.chain().undo().run(),
+        onClick: () => editor?.chain().focus().undo().run(),
       },
+      {
+        label: "Redo",
+        icon: Redo2Icon,
+        onClick: () => editor?.chain().focus().redo().run(),
+      },
+      {
+        label: "Print",
+        icon: PrinterIcon,
+        onClick: () => window.print(),
+      },
+      {
+        label: "Spell Check",
+        icon: SpellCheckIcon,
+        onClick: () => {
+          const current = editor?.view.dom.getAttribute("spellcheck");
+          editor?.view.dom.setAttribute(
+            "spellcheck",
+            current === "false" ? "true" : "false"
+          );
+        },
+      },
+    ],
+    [
+      {
+        label: "Bold",
+        icon: BoldIcon,
+        isActive: editor?.isActive("bold"),
+        onClick: () => editor?.chain().focus().toggleBold().run(),
+      },
+      {
+        label: "Italic",
+        icon: ItalicIcon,
+        isActive: editor?.isActive("italic"),
+        onClick: () => editor?.chain().focus().toggleItalic().run(),
+      },
+      {
+        label: "Underline",
+        icon: UnderlineIcon,
+        isActive: editor?.isActive("underline"),
+        onClick: () => editor?.chain().focus().toggleUnderline().run(),
+      },
+    ],
+    [
+      {
+        label: "Comment",
+        icon: MessageSquarePlusIcon,
+        onClick: () => console.log("TODO:comment"),
+        isActive: false, // TODO: 开启此功能激活
+      },
+      {
+        label: "ListTodo",
+        icon: ListTodoIcon,
+        onClick: () => editor?.chain().focus().toggleTaskList().run(),
+        isActive: editor?.isActive("taskList"),
+      },
+      {
+        label: 'Remove Formatting',
+        icon: RemoveFormattingIcon,
+        onClick:()=>editor?.chain().focus().unsetAllMarks().run(),
+      }
     ],
   ];
 
   return (
     <div className="bg-[#F1F4F9] px-2.5 py-0.5 rounded-[24px] min-h-[40px] flex items-center gap-x-0.5 overflow-x-auto">
       {sections[0].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      <Separator orientation="vertical" className="h-6 bg-neutral-200" />
+      {/* TODO: 字体主题 */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-200" />
+      {/* TODO: 标题 */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-200" />
+      {/* TODO: 字体大小 */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-200" />
+      {sections[1].map((item) => (
+        <ToolbarButton key={item.label} {...item} />
+      ))}
+      {/* TODO: 文本颜色 */}
+      {/* TODO: 文本高亮颜色 */}
+      <Separator orientation="vertical" className="h-6 bg-neutral-200" />
+      {/* TODO: 链接 */}
+      {/* TODO: 图片 */}
+      {/* TODO: 排列 */}
+      {/* TODO: 行高 */}
+      {/* TODO: 列表 */}
+      {sections[2].map((item) => (
         <ToolbarButton key={item.label} {...item} />
       ))}
     </div>
